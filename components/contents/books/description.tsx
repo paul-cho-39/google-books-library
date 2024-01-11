b190586289b309f53ac825b80d99b9629a6e9550
Paul Cho <chosung2loud@gmail.com>
2024-01-10 18:50:34 -0800

    change styling and adding documentation

---
+++ components/contents/books/description.tsx
@@ -0,0 +1,29 @@
+import SectionHeader from '@/components/headers/sectionHeader';
+import { Suspense, lazy } from 'react';
+
+const BookDescription = lazy(() => import('@/components/bookcover/description'));
+
+interface DescriptionSectionProps {
+   description: string | undefined;
+}
+
+const DescriptionSection = ({ description }: DescriptionSectionProps) => {
+   return (
+      <section id='description'>
+         <div role='contentinfo' id='book-info' className='my-4 w-full py-2 lg:my-12'>
+            <SectionHeader title='Description' />
+            <Suspense fallback={null}>
+               <BookDescription
+                  description={description}
+                  descriptionLimit={250}
+                  textSize='text-lg'
+                  isLink={false}
+                  href={''}
+               />
+            </Suspense>
+         </div>
+      </section>
+   );
+};
+
+export default DescriptionSection;
